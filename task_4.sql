a176a4102dab86b8d1e8c70ec61ac03b8ed5aecc
Msiza-VL <lindokehlemsiza.00@gmail.com>
2024-10-19 10:01:29 +0200

    prints the full description of the table books from the database alx_book_store

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,16 @@
+-- Use the alx_book_store database
+USE alx_book_store;
 
+-- Query to get the full description of the books table
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+    EXTRA 
+FROM 
+    information_schema.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' AND 
+    TABLE_NAME = 'Books';
